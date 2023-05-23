5786aa5b2ae1748e3636fa3fc704a65ead19daa5
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-23 23:41:06 +0100

    Overview and Operations components built. Components are fully responsive. Lack data fetching functionality

--- src/components/Blok/sub components/BlokOperations.jsx
+++ src/components/Blok/sub components/BlokOperations.jsx
@@ -5,8 +5,8 @@ import IndrasArrow from '../../../assets/Blok/images/IndrasArrow.svg'
 import Ethereum from '../../../assets/Blok/images/ethereum.svg'
 const BlokOperations = () => {
     return (
-        <div className=' w-mxl:flex xl:w-full xl:max-w-[430px] border border-blok-color rounded-16 p-4 grid gap-5 sm:flex sm:p-6 md:grid'>
-            <div className="flex flex-col gap-3 md:flex-row md:justify-start xl:justify-between">
+        <div className='border border-blok-color rounded-16 p-4 grid gap-5 sm:flex sm:p-6 md:grid'>
+            <div className="flex flex-col gap-3 md:flex-row md:justify-start md:items-center xl:justify-between xl:h-fit">
                 <p className='text-[20px] text-blok-dark font-cabinet font-bold'>Operations</p>
 
                 <div className='flex border border-blok-color justify-between px-2 py-1 rounded-8 text-12 text-blok-grey bg-[#f6f6f6] max-w-fit'>
@@ -16,13 +16,13 @@ const BlokOperations = () => {
                 </div>
             </div>
 
-            <div className="grid gap-3 sm:w-full md:flex md:justify-start md:items-end">
-                <div className="grid gap-2">
+            <div className="grid gap-3 sm:w-full md:flex md:justify-stretch md:items-end xl:max-w-[430px] xl:grid">
+                <div className="grid gap-2 md:min-w-[200px]">
                     <span>You pay</span>
-                    <div className='py-2 px-3 bg-[#F6F6F6] border border-blok-color rounded-8 flex justify-between items-center text-14 font-cabinet font-bold md:min-w-[200px]'>
+                    <div className='py-2 px-3 bg-[#F6F6F6] border border-blok-color rounded-8 flex justify-between items-center text-14 font-cabinet font-bold'>
                         <div className="bg-white py-2 rounded-8 px-2 flex justify-center items-center gap-2">
                             <img src={Tether} alt="" className='w-5 h-5' />
-                            <span className=''>ETH</span>
+                            <span className=''>USDT</span>
                             <button>
                                 <img src={ChevronFilledDown} alt="" />
                             </button>
@@ -33,11 +33,11 @@ const BlokOperations = () => {
                     </div>
                 </div>
 
-                <span className='relative'><img src={IndrasArrow} alt="" className='mx-auto md:-translate-y-4 md:rotate-90' /></span>
+                <span className='relative'><img src={IndrasArrow} alt="" className='mx-auto md:-translate-y-4 md:rotate-90 xl:translate-y-4 xl:rotate-0' /></span>
 
-                <div className="grid gap-2">
+                <div className="grid gap-2 md:min-w-[200px]">
                     <span>You get</span>
-                    <div className='py-2 px-3 bg-[#F6F6F6] border border-blok-color rounded-8 flex justify-between items-center text-14 font-cabinet font-bold md:min-w-[200px]'>
+                    <div className='py-2 px-3 bg-[#F6F6F6] border border-blok-color rounded-8 flex justify-between items-center text-14 font-cabinet font-bold'>
                         <div className="bg-white py-2 rounded-8 px-2 flex justify-center items-center gap-2">
                             <img src={Ethereum} alt="" className='w-5 h-5' />
                             <span className=''>ETH</span>
@@ -48,10 +48,10 @@ const BlokOperations = () => {
 
                         <span>$1300.00</span>
                     </div>
-                <span className='md:absolute md:translate-x-[200%]'>{`1${' ETH '} = ${' $1300.08'}`}</span>
+                    <span className='font-cabinet font-bold text-blok-grey md:absolute md:translate-x-[200%] xl:relative xl:translate-x-0'>{`1${' ETH '} = ${' $1300.08'}`}</span>
                 </div>
 
-                <div className="md:min-w-[200px]">
+                <div className="flex-grow">
                     <button className='w-full flex justify-center items-center text-white bg-blok-dark-green h-14 rounded-8 text-14  font-bold font-cabinet'>Buy Ethereum</button>
                 </div>
             </div>
