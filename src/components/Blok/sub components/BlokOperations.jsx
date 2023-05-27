952f11c7d47b184ff6b57827a437aa5b166b19a4
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-27 21:35:19 +0100

    BlokOperation component fully responsive

--- src/components/Blok/sub components/BlokOperations.jsx
+++ src/components/Blok/sub components/BlokOperations.jsx
@@ -4,16 +4,16 @@ import IndrasArrow from '../../../assets/Blok/images/IndrasArrow.svg'
 import Ethereum from '../../../assets/Blok/images/Ethereum.svg'
 import { useBlokContext } from '../../../context/BlokContext'
 import { BlokOperation } from '../../../assets/Blok/data'
-import {BsCurrencyDollar} from 'react-icons/bs'
+import { BsCurrencyDollar } from 'react-icons/bs'
 
 const BlokOperations = () => {
-    const {currentOperation, setCurrentOperation} = useBlokContext()
+    const { currentOperation, setCurrentOperation } = useBlokContext()
     return (
-        <div className='border border-blok-color rounded-16 p-4 grid gap-5 sm:flex sm:p-6 md:grid'>
-            <div className="flex flex-col gap-3 md:flex-row md:justify-start md:items-center xl:justify-between xl:h-fit">
+        <div className='border border-blok-color rounded-16 p-4 flex flex-col gap-5 sm:flex-row mdl:flex-col lg:flex-row xl:flex-col sm:p-6'>
+            <div className="flex flex-col gap-3 mdl:flex-row mdl:items-center mdl:gap-5 lg:flex-col lg:items-start lg:gap-3 xl:flex-row xl:justify-between xl:items-center h-fit">
                 <p className='text-[20px] text-blok-dark dark:text-white font-cabinet font-bold'>Operations</p>
 
-                <div className='flex border border-blok-color justify-between px-2 py-1 rounded-8 text-12 text-blok-grey bg-[#f6f6f6] max-w-fit'>
+                <div className='flex border border-blok-color justify-between px-2 py-1 rounded-8 text-12 text-blok-grey bg-[#f6f6f6] max-w-fit h-[50px]'>
                     {
                         BlokOperation[0].operations.map((operation, index) => (
                             <button
@@ -29,43 +29,51 @@ const BlokOperations = () => {
                 </div>
             </div>
 
-            <form className="grid gap-3 sm:w-full md:flex md:justify-stretch md:items-end xl:max-w-[430px] xl:grid">
-                <div className="grid gap-2 md:min-w-[200px]">
-                    <span className='dark:text-white'>You pay</span>
-                    <div className='py-2 px-3 bg-[#F6F6F6] border border-blok-color rounded-8 flex justify-between items-center text-14 font-cabinet font-bold'>
-                        <button className="bg-white py-2 rounded-8 px-2 flex justify-center items-center gap-2" value={'USD'}>
-                            <BsCurrencyDollar className='w-5 h-5' />
-                            <span className=''>USD</span>
-                            <span>
-                                <img src={ChevronFilledDown} alt="" />
-                            </span>
-                        </button>
+            <form className="flex flex-col gap-3 sm:w-full sm:mt-[10px] mdl:flex-row mdl:items-end mdl:mb-5 xl:mb-0 lg:flex-col lg:items-center xl:max-w-[430px] xl:grid">
 
-                        <input type="number" name="amount_paid" id="amount_paid" className='h-10 px-3 text-end text-16 rounded-8 focus:ring-0 focus:outline-transparent focus:border-transparent border-1' placeholder='Enter an amount' defaultValue={4200}/>
+                <div className="flex flex-col gap-2 md:min-w-[200px] lg:w-full">
+                    <span className='dark:text-white'>You pay</span>
+
+                    <div className='p-1 lg:p-2 bg-[#F6F6F6] border border-blok-color rounded-8 flex gap-2 justify-between items-center text-14 font-cabinet font-bold'>
+                        <button className="bg-white py-2 rounded-8 px-2 flex justify-center items-center gap-2 w-fit" value={'USD'}>
+                            <BsCurrencyDollar className='w-4 h-4 flex-shrink-0' />
+                            <span className=''>USD</span>
+                            <div className='w-5 h-5'>
+                                <img src={ChevronFilledDown} className='flex-shrink-0 w-5 h-5 invisible' alt="" />
+                            </div>
+                        </button>
+                        <div className="">
+
+                            <input type="number" name="amount_paid" id="amount_paid" className='h-10 px-3 text-end text-16 rounded-8 focus:ring-0 focus:outline-transparent focus:border-transparent border-1 w-[100%] placeholder:text-14 lg:placeholder:text-16' placeholder='Enter an amount' />
+                        </div>
 
                     </div>
                 </div>
 
-                <span className='relative'><img src={IndrasArrow} alt="" className='mx-auto md:-translate-y-4 md:rotate-90 xl:translate-y-4 xl:rotate-0' /></span>
+                <span className='relative'>
+                    <img src={IndrasArrow} alt="" className='mx-auto mdl:rotate-90 mdl:-translate-y-4 lg:rotate-0 lg:-translate-y-0 xl:rotate-0' />
+                </span>
 
-                <div className="grid gap-2 md:min-w-[200px]">
+                <div className="flex flex-col gap-2 md:min-w-[200px] lg:w-full">
                     <span className='dark:text-white'>You get</span>
-                    <div className='py-2 px-3 bg-[#F6F6F6] border border-blok-color rounded-8 flex justify-between items-center text-14 font-cabinet font-bold'>
-                        <button className="bg-white py-2 rounded-8 px-2 flex justify-center items-center gap-2">
-                            <img src={Ethereum} alt="" className='w-5 h-5' />
+                    <div className='p-1 bg-[#F6F6F6] border border-blok-color rounded-8 flex gap-2 justify-between items-center text-14 font-cabinet font-bold'>
+                        <button className="bg-white py-2 rounded-8 px-2 flex justify-center items-center gap-2 w-fit" value={'USD'}>
+                            <div className='w-5 h-5'><img src={Ethereum} className='flex-shrink-0' /></div>
                             <span className=''>ETH</span>
-                            <span>
-                                <img src={ChevronFilledDown} alt="" />
-                            </span>
+                            <div className='w-5 h-5'>
+                                <img src={ChevronFilledDown} className='flex-shrink-0 w-5 h-5' alt="" />
+                            </div>
                         </button>
+                        <div className="">
+                            <input type="number" name="amount_paid" id="amount_paid" className='h-10 px-3 text-end text-16 rounded-8 focus:ring-0 focus:outline-transparent focus:border-transparent border-1 w-[100%] placeholder:text-14' defaultValue={0} />
+                        </div>
 
-                        <input type="number" name="asset_recieved" id="asset_recieved" className='h-10 px-3 text-end text-16 rounded-8 focus:ring-0 focus:outline-transparent focus:border-transparent border-1' defaultValue={3.123} />
                     </div>
-                    <span className='font-cabinet font-bold text-blok-grey md:absolute md:translate-x-[200%] xl:relative xl:translate-x-0'>{`1${' ETH '} = ${' $1300.08'}`}</span>
+                    <span className='font-cabinet font-bold text-blok-grey mdl:absolute mdl:translate-y-[90px] lg:relative lg:translate-y-[0] xl:relative xl:translate-x-0'>{`1${' ETH '} = ${' $1300.08'}`}</span>
                 </div>
 
                 <div className="flex-grow">
-                    <button className='w-full flex justify-center items-center text-white bg-blok-dark-green h-14 rounded-8 text-14  font-bold font-cabinet'>Buy Ethereum</button>
+                    <button className='w-full flex justify-center items-center min-w-[200px] text-white bg-blok-dark-green h-[50px] rounded-8 text-14 font-bold font-cabinet'>Buy Ethereum</button>
                 </div>
             </form>
         </div>
