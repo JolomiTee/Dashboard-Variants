227abdd14c49bb4651cbc6a0137d37de43708f65
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-27 13:49:01 +0100

    Created new files to dispay various BlokOperation: Buy, Sell, Exchange

---
+++ src/components/Blok/sub components/operations/BlokBuy.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlokBuy = () => {
+  return (
+    <div>BlokBuy</div>
+  )
+}
+
+export default BlokBuy
---
+++ src/components/Blok/sub components/operations/BlokSell.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlokSell = () => {
+  return (
+    <div>BlokSell</div>
+  )
+}
+
+export default BlokSell
