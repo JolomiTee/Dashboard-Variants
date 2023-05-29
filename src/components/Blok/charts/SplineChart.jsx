06aa1f5204aa8353bc5ddc31e3f2a33c955b8c80
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-29 01:13:49 +0100

    Installed the TradingView lightweight-chart library. generated default data for Analysis Chart display

--- src/components/Blok/charts/SplineChart.jsx
+++ src/components/Blok/charts/SplineChart.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import axios from "axios";
+
+
 
 const SplineChart = () => {
-  return (
-    <div>SplineChart</div>
-  )
+    const apiFetch = async () => {
+        let headersList = {
+            "Accept": "*/*",
+            "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+            "X-CoinAPI-Key": "67CE7602-E47C-4E5B-AADF-AED1FD516B77"
+        }
+
+        let reqOptions = {
+            url: "https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_USDT_USD/latest?period_id=30MIN",
+            method: "GET",
+            headers: headersList,
+        }
+
+        let response = await axios.request(reqOptions);
+        console.log(response.data);
+
+    }
+
+    return (
+        <div>SplineChart</div>
+    )
 }
 
 export default SplineChart
